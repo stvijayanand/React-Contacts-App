07db3a4a544a7866792e5d0e1010c029b70e39f8
Vijayanand Settin <stvijayanand@hotmail.com>
2019-03-02 13:19:36 -0800

    test check-in

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
-import React, { Component } from 'react';
-import ListContacts from "./ListContacts"
+import React, { Component } from "react";
+import ListContacts from "./ListContacts";
 
 class App extends Component {
-  state={
+  state = {
     contacts: [
       {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
+        id: "karen",
+        name: "Karen Isgrigg",
+        handle: "karen_isgrigg",
+        avatarURL: "http://localhost:5001/karen.jpg"
       },
       {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
+        id: "richard",
+        name: "Richard Kalehoff",
+        handle: "richardkalehoff",
+        avatarURL: "http://localhost:5001/richard.jpg"
       },
       {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
+        id: "tyler",
+        name: "Tyler McGinnis",
+        handle: "tylermcginnis",
+        avatarURL: "http://localhost:5001/tyler.jpg"
       }
-     ]
-  }
+    ]
+  };
 
   removeContact = contactToRemove => {
-    this.setState((currentState) => ({
-      contacts: currentState.contacts.filter(contact => contact.id !== contactToRemove.id)
+    this.setState(currentState => ({
+      contacts: currentState.contacts.filter(
+        contact => contact.id !== contactToRemove.id
+      )
     }));
-  }
+  };
 
   render() {
-    const { contacts } = this.state.contacts;
-
     return (
       <div>
-        <ListContacts 
-        contacts={contacts}
-        onRemoveContact={this.removeContact}></ListContacts>    
+        <ListContacts
+          contacts={this.state.contacts}
+          onRemoveContact={this.removeContact}
+        />
       </div>
     );
   }
